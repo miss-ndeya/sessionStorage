388eddaf7ab7f7aea8eb239aa7ac339a04cbc1c5
unknown <miss-ndeya>
2023-07-11 11:33:13 +0200

    ajouter

---
+++ sscript.js
@@ -0,0 +1,17 @@
+// fonction pour incrementer le chiffre
+function incrementer() {
+    if (sessionStorage.getItem('chiffre')) {
+        let chiffre = parseInt(sessionStorage.getItem('chiffre'));
+        chiffre += 1;
+        sessionStorage.setItem('chiffre', chiffre.toString());
+    } else {
+        sessionStorage.setItem('chiffre', '1');
+    }
+    document.getElementById('chiffre').innerText =  'Voici un chiffre ' + sessionStorage.getItem('chiffre');
+}
+document.getElementById('chiffre').innerText =  'Voici un chiffre ' + sessionStorage.getItem('chiffre');
+
+
+
+
+
